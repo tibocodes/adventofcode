da564e8cd6ca4b182f0fd4b3c6841e5c703a3c50
tibocodes <tibo@codes.com>
2020-12-09 22:10:13 +0100

    exo2

--- 2020/day_9/src/main.rs
+++ 2020/day_9/src/main.rs
@@ -3,15 +3,35 @@ use std::io::BufReader;
 use std::io::prelude::*;
 use std::fs::File;
 
+// Exo1
+// fn main() {
+//     let values = values();
+//     for (i, value) in values[25..].iter().enumerate() {
+//         let window = &values[i..i + 25];
+//         if sum_in(window, value) == false {
+//             println!("result : {}", value);
+//             break;
+//         }
+//     }
+// }
+
+// Exo2
+// brute force, should at least opt out of loop when the sum is already > to the expected result
 fn main() {
     let values = values();
-    for (i, value) in values[25..].iter().enumerate() {
-        let window = &values[i..i + 25];
-        if sum_in(window, value) == false {
-            println!("result : {}", value);
-            break;
+    for (i, _) in values.iter().enumerate() {
+        for (j, _) in values[i..].iter().enumerate() {
+            let sum: u64 = values[i..(i + j)].iter().sum();
+            if sum == 756008079 {
+                let min = values[i..(i + j)].iter().min().unwrap();
+                let max = values[i..(i + j)].iter().max().unwrap();
+                println!("min: {} max:{}", min, max);
+                println!("sum: {}", min + max);
+                return;
+            }
         }
-    }
+
+    } 
 }
 
 
